4a00ce9d1b4e34d3ac7bb8c92b94340214df6b9b
Lizzie Siegle <esiegle@brynmawr.edu>
2022-10-28 11:11:58 -0700

    clean up code

--- functions/congress.js
+++ functions/congress.js
@@ -9,25 +9,20 @@ exports.handler = async (context, event, callback) => {
     const data = await body.json();
     const billsLength = data.bills.length;
     const randNum = Math.floor(Math.random() * (billsLength - 1)) + 1;
-    const latestActionDate = data.bills[randNum].latestAction.actionDate;
+    const bill = data.bills[randNum];
+    const { latestAction, title, type, number, originChamber } = bill;
     
-    const latestActionTxt = data.bills[randNum].latestAction.text;
-    const title = data.bills[randNum].title;
-    const congress = data.bills[randNum].congress;
-    const type = data.bills[randNum].type;
-    const num = data.bills[randNum].number;
-    const originChamber = data.bills[randNum].originChamber;
     const billMap = {
-      HR: "house bill", 
-      S: "senate bill",
-      HJRES: "house joint resolution",
-      SJRES: "senate joint resolution",
-      HCONRES: "house concurrent resolution",
-      SCONRES: "senate concurrent resolution",
-      HRES: "house simple resolution",
-      SRES: "senate simple resolution"
+        HR: "house bill",
+        S: "senate bill",
+        HJRES: "house joint resolution",
+        SJRES: "senate joint resolution",
+        HCONRES: "house concurrent resolution",
+        SCONRES: "senate concurrent resolution",
+        HRES: "house simple resolution",
+        SRES: "senate simple resolution"
     };
-    twiml.message(`Bill: ${title}.\nType: ${billMap[type]}\nAssigned bill or resolution number: ${num}\nIt originated in the ${originChamber} but the latest action was ${latestActionTxt} on ${latestActionDate}`);
+    twiml.message(`Bill: ${title}.\nType: ${billMap[type]}\nAssigned bill or resolution number:   ${number}\nIt originated in the ${originChamber} but the latest action was ${latestAction.text} on ${latestAction.actionDate}`);
   }
   else if(inbMsg.includes("amendment")) {
     const {
@@ -36,13 +31,9 @@ exports.handler = async (context, event, callback) => {
     const data = await body.json();
     const amendmentLength = data.amendments.length;
     const randNum = Math.floor(Math.random() * (amendmentLength - 1)) + 1;
-    const latestActionDate = data.amendments[randNum].latestAction.actionDate;
-    const latestActionTxt = data.amendments[randNum].latestAction.text;
-    const purpose = data.amendments[randNum].purpose;
-    const congress = data.amendments[randNum].congress;
-    const type =  data.amendments[randNum].type;
-    const number = data.amendments[randNum].number;
-    twiml.message(`Amendment purpose: ${purpose}\nAssigned amendment or resolution number:${number}\n Latest action was on ${latestActionDate}.\n The bill was ${latestActionTxt}\n Congress: ${congress}.`);
+    const amendment = data.amendments[randNum];
+    const { latestAction, purpose, congress, number } = amendment;
+    twiml.message(`Amendment purpose: ${purpose}\nAssigned amendment or resolution number:${number}\n Latest action was on ${latestAction.actionDate} to ${latestAction.text}.\n Congress: ${congress}.`);
   }
   else if(inbMsg.includes("summaries")) {
     const CURRENT_CONGRESS = 117;
@@ -52,16 +43,18 @@ exports.handler = async (context, event, callback) => {
     const data = await body.json();
     const sumLength = data.summaries.length;
     const randNum = Math.floor(Math.random() * (sumLength - 1)) + 1;
-    const currentChamber = data.summaries[randNum].currentChamber;
-    const billCongressNum = data.summaries[randNum].bill.congress;
-    const originChamber = data.summaries[randNum].bill.originChamber;
-    const billTitle = data.summaries[randNum].bill.title;
+    const summaries = data.summaries[randNum];
+    const { currentChamber, billCongressNum, originChamber, bill } = summaries;
+    //const currentChamber = data.summaries[randNum].currentChamber;
+    //const billCongressNum = data.summaries[randNum].bill.congress;
+    //const originChamber = data.summaries[randNum].bill.originChamber;
+    //const billTitle = data.summaries[randNum].bill.title;
     let text = data.summaries[randNum].text;
     const regexToStripHtmlTags = /<(.|\n)*?>/g;
     const textWithoutHtmlTags = text.replace(regexToStripHtmlTags, '');
     const actionDate = data.summaries[randNum].actionDate;
     const actionDesc = data.summaries[randNum].actionDesc;
-    const msg = `Summary title: ${billTitle}.\nStarted in: ${originChamber} on ${actionDate}, currently in the ${currentChamber} chamber in Congress ${billCongressNum} and it was ${actionDesc}\n${textWithoutHtmlTags}`;
+    const msg = `Summary title: ${bill.title}.\nStarted in: ${bill.originChamber} on ${actionDate}, currently in the ${currentChamber} chamber in Congress ${bill.congress} and it was ${actionDesc}\n${textWithoutHtmlTags}`;
     twiml.message(msg);
   }
   else {
